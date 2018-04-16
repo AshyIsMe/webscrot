b7d96557b98f102673540b92ac078805df7d8b8e
Aaron Colby Mervin Ash <aaron.ash@gmail.com>
2018-04-16 22:33:19 +1000

    WIP: command-line-args

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Screenshot a webpage similar to scrot for desktop screenshots
 $ webscrot.js https://earth.nullschool.net
 $ xdg-open 2018-04-16-185222_1920x1080_webscrot.png
 ```
+
+## TODO
+
+* The rest of the scrot style features that make sense:
+  * --exec APP
+  * Special Strings for --exec and filename
+* Put it up on AUR
--- webscrot.js
+++ webscrot.js
@@ -2,6 +2,7 @@
 
 const puppeteer = require('puppeteer');
 const moment = require('moment');
+const commandLineUsage = require('command-line-usage')
 
 var now = moment().format('YYYY-MM-DD-HHmmss');
 var url = 'https://earth.nullschool.net';
@@ -9,6 +10,27 @@ var waitForMs = 2000;
 var width = 1920;
 var height = 1080;
 var outfilename = `${now}_${width}x${height}_webscrot.png`;
+
+const usage = `
+Usage: webscrot.js [OPTIONS]... URL [FILE]
+Usage : scrot [OPTIONS]... [FILE]
+  Where URL is the url to load in headless chrome for the screenshot.
+  Where FILE is the target file for the screenshot.
+  If FILE is not specified, a date-stamped file will be dropped in the
+  current directory.
+  -h, --help                display this help and exit
+  -v, --version             output version information and exit
+  -d, --delay NUM           wait NUM seconds before taking a shot
+  -w, --width NUM           viewport width NUM pixels wide
+  -h, --height NUM          viewport height NUM pixels high
+
+  Example:
+          webscrot.js 'https://earth.nullschool.net' 'earthnullschool_webscrot.png'
+
+This program is free software see the file COPYING for licensing info.
+Copyright Aaron Ash 2018
+`
+
 
 (async () => {
   const browser = await puppeteer.launch();
