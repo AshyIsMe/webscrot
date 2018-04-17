a07316055ae7b1b0e499ee19fef8397897a21385
Aaron Ash <aaron.ash@gmail.com>
2018-04-17 10:27:53 +1000

    Added commandline args

--- README.md
+++ README.md
@@ -2,11 +2,32 @@
 
 Screenshot a webpage similar to scrot for desktop screenshots
 
+## Install
+```
+$ git clone https://github.com/ashyisme/webscrot && cd webscrot
+$ npm i .
+```
+
 ## Usage
 
 ```
-$ webscrot.js https://earth.nullschool.net
-$ xdg-open 2018-04-16-185222_1920x1080_webscrot.png
+Usage: webscrot.js [OPTIONS]... URL [FILE]
+Usage : scrot [OPTIONS]... [FILE]
+  Where URL is the url to load in headless chrome for the screenshot.
+  Where FILE is the target file for the screenshot.
+  If FILE is not specified, a date-stamped file will be dropped in the
+  current directory.
+  -h, --help                display this help and exit
+  -v, --version             output version information and exit
+  -d, --delay NUM           wait NUM seconds before taking a shot
+  -W, --width NUM           viewport width NUM pixels wide
+  -H, --height NUM          viewport height NUM pixels high
+
+  Example:
+          webscrot.js 'https://earth.nullschool.net' screenshot.png
+
+This program is free software see the file COPYING for licensing info.
+Copyright Aaron Ash 2018
 ```
 
 ## TODO
--- webscrot.js
+++ webscrot.js
@@ -2,16 +2,10 @@
 
 const puppeteer = require('puppeteer');
 const moment = require('moment');
-const commandLineUsage = require('command-line-usage')
+const commandLineArgs = require('command-line-args')
 
-var now = moment().format('YYYY-MM-DD-HHmmss');
-var url = 'https://earth.nullschool.net';
-var waitForMs = 2000;
-var width = 1920;
-var height = 1080;
-var outfilename = `${now}_${width}x${height}_webscrot.png`;
-
-const usage = `
+function usage() {
+	const help = `
 Usage: webscrot.js [OPTIONS]... URL [FILE]
 Usage : scrot [OPTIONS]... [FILE]
   Where URL is the url to load in headless chrome for the screenshot.
@@ -21,24 +15,60 @@ Usage : scrot [OPTIONS]... [FILE]
   -h, --help                display this help and exit
   -v, --version             output version information and exit
   -d, --delay NUM           wait NUM seconds before taking a shot
-  -w, --width NUM           viewport width NUM pixels wide
-  -h, --height NUM          viewport height NUM pixels high
+  -W, --width NUM           viewport width NUM pixels wide
+  -H, --height NUM          viewport height NUM pixels high
 
   Example:
-          webscrot.js 'https://earth.nullschool.net' 'earthnullschool_webscrot.png'
+          webscrot.js 'https://earth.nullschool.net' screenshot.png
 
 This program is free software see the file COPYING for licensing info.
 Copyright Aaron Ash 2018
-`
+`;
+	console.log(help);
+	process.exit();
+}
 
+const optionDefinitions = [
+	  { name: 'help'    ,  alias: 'h' ,  type: Boolean },
+	  { name: 'version' ,  alias: 'v' ,  type: Boolean },
+	  { name: 'delay'   ,  alias: 'd' ,  type: Number , defaultValue: 0 },
+	  { name: 'width'   ,  alias: 'W' ,  type: Number , defaultValue: 1920 },
+	  { name: 'height'  ,  alias: 'H' ,  type: Number , defaultValue: 1080 },
+	  { name: 'url'     , type: String , defaultOption: true, defaultValue: '' },
+]
+
+const positionalOptionDefinitions = [
+	  { name: 'outfile', alias: 'o', type: String, defaultOption: true },
+]
+
+const mainOptions = commandLineArgs(optionDefinitions, { stopAtFirstUnknown: true })
+const argv = mainOptions._unknown || []
+
+const positionalOptions = commandLineArgs(positionalOptionDefinitions, { argv, stopAtFirstUnknown: true  })
+
+var now = moment().format('YYYY-MM-DD-HHmmss');
+var outfilename = positionalOptions.outfile || `${now}_${mainOptions.width}x${mainOptions.height}_webscrot.png`;
+
+
+if(mainOptions.version) {
+	var pjson = require('./package.json');
+	console.log(pjson.version);
+	process.exit();
+}
+if(mainOptions.help) {
+	usage();
+}
+if(mainOptions.url == '') {
+	usage();
+}
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({width: width, height: height});
-  await page.goto(url, {waitUntil: 'networkidle2'});
-  //await page.goto(url);
-  await page.waitFor(waitForMs);
+  await page.setViewport({width: mainOptions.width, height: mainOptions.height});
+  await page.goto(mainOptions.url, {waitUntil: 'networkidle2'});
+  //await page.goto(mainOptions.url);
+  await page.waitFor(mainOptions.delay * 1000);
   await page.screenshot({path: outfilename});
 
   await browser.close();
